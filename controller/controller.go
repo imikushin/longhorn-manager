e976fbba8c32f6e90eb2bf9c3eec918687a4e766
Ivan Mikushin <i.mikushin@gmail.com>
2017-02-04 12:39:14 -0800

    WIP: controller

--- controller/controller.go
+++ controller/controller.go
@@ -2,22 +2,29 @@ package controller
 
 import (
 	"fmt"
+	"github.com/pkg/errors"
 	"github.com/rancher/longhorn-orc/types"
+	"github.com/rancher/longhorn/controller/client"
 	"github.com/rancher/longhorn/sync"
 )
 
 type controller struct {
-	url string
+	name   string
+	url    string
+	client *client.ControllerClient
 }
 
 func New(volume *types.VolumeInfo) types.Controller {
+	url := fmt.Sprintf("http://%s:9501", volume.Controller.Address)
 	return &controller{
-		url: fmt.Sprintf("http://%s:9501", volume.Controller.Address),
+		name:   volume.Name,
+		url:    url,
+		client: client.NewControllerClient(url),
 	}
 }
 
 func (c *controller) Name() string {
-	return "DUMMY"
+	return c.name
 }
 
 func (c *controller) GetReplicaStates() ([]*types.ReplicaInfo, error) {
@@ -25,9 +32,11 @@ func (c *controller) GetReplicaStates() ([]*types.ReplicaInfo, error) {
 }
 
 func (c *controller) AddReplica(replica *types.ReplicaInfo) error {
-	return sync.NewTask(c.url).AddReplica(replica.Address)
+	err := sync.NewTask(c.url).AddReplica(replica.Address)
+	return errors.Wrapf(err, "failed to add replica '%s' IP='%s' to controller '%s'", replica.ID, replica.Address, c.name)
 }
 
 func (c *controller) RemoveReplica(replica *types.ReplicaInfo) error {
-	return nil
+	_, err := c.client.DeleteReplica(replica.Address)
+	return errors.Wrapf(err, "failed to rm replica '%s', IP='%s' from controller '%s'", replica.ID, replica.Address, c.name)
 }
