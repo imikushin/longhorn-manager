ac58048a10c191c00d36c292ac0dfc4f0a4a0053
Ivan Mikushin <i.mikushin@gmail.com>
2017-02-07 15:29:26 -0800

    WIP: cattle

--- orch/cattle/cattle.go
+++ orch/cattle/cattle.go
@@ -5,8 +5,15 @@ import (
 	"github.com/pkg/errors"
 	"github.com/rancher/go-rancher-metadata/metadata"
 	client "github.com/rancher/go-rancher/v2"
+	"github.com/rancher/longhorn-orc/orch"
 	"github.com/rancher/longhorn-orc/types"
+	"github.com/rancher/rancher-compose/rancher"
 	"github.com/urfave/cli"
+	"strconv"
+)
+
+const (
+	LastReplicaIndexField = "lastReplicaIndex"
 )
 
 type cattleOrc struct {
@@ -14,10 +21,12 @@ type cattleOrc struct {
 	metadata metadata.Client
 
 	hostID string
+
+	LonghornImage, OrcImage string
 }
 
 func New(c *cli.Context) types.Orchestrator {
-	rancher, err := client.NewRancherClient(&client.ClientOpts{
+	rancherClient, err := client.NewRancherClient(&client.ClientOpts{
 		Url:       c.GlobalString("cattle-url"),
 		AccessKey: c.GlobalString("cattle-access-key"),
 		SecretKey: c.GlobalString("cattle-secret-key"),
@@ -31,13 +40,46 @@ func New(c *cli.Context) types.Orchestrator {
 		logrus.Fatalf("%+v", errors.Wrap(err, "failed to get self host from rancher metadata"))
 	}
 	return &cattleOrc{
-		rancher:  rancher,
-		metadata: md,
-		hostID:   host.UUID,
+		rancher:       rancherClient,
+		metadata:      md,
+		hostID:        host.UUID,
+		LonghornImage: c.GlobalString(orch.LonghornImageParam),
+		OrcImage:      c.GlobalString(orch.OrcImageParam),
 	}
 }
 
+func volumeStackName(name string) string {
+	return "volume-" + name
+}
+
+func replicaName(i int) string {
+	return "replica-" + strconv.Itoa(i)
+}
+
 func (orc *cattleOrc) CreateVolume(volume *types.VolumeInfo) (*types.VolumeInfo, error) {
+	stack0 := &client.Stack{
+		Name:          volumeStackName(volume.Name),
+		System:        true,
+		StartOnCreate: false,
+		Outputs:       map[string]interface{}{LastReplicaIndexField: "0"},
+	}
+	_, err := orc.rancher.Stack.Create(stack0)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to create stack '%s'", stack0.Name)
+	}
+
+	replicas := map[int]*types.ReplicaInfo{}
+	for i := 1; i <= volume.NumberOfReplicas; i++ {
+		replicas[i] = &types.ReplicaInfo{Name: replicaName(i)}
+	}
+	//lastReplicaIndex := volume.NumberOfReplicas
+
+	// TODO create replicas
+
+	context := &rancher.Context{}
+	project := rancher.NewProject(context)
+	project.Parse()
+
 	return nil, nil
 }
 
