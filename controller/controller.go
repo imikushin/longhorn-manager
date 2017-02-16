419d43ce63d532a8fbdc42486fcb28c34fa14b5a
Ivan Mikushin <i.mikushin@gmail.com>
2017-02-15 19:24:42 -0800

    controller interface: use longhorn CLI

--- controller/controller.go
+++ controller/controller.go
@@ -3,22 +3,22 @@ package controller
 import (
 	"github.com/pkg/errors"
 	"github.com/rancher/longhorn-orc/types"
-	"github.com/rancher/longhorn/controller/client"
-	"github.com/rancher/longhorn/sync"
+	"github.com/rancher/longhorn-orc/util"
+	"os/exec"
+	"strings"
+	"sync"
 )
 
 type controller struct {
-	name   string
-	url    string
-	client *client.ControllerClient
+	name string
+	url  string
 }
 
 func New(volume *types.VolumeInfo) types.Controller {
 	url := volume.Controller.Address
 	return &controller{
-		name:   volume.Name,
-		url:    url,
-		client: client.NewControllerClient(url),
+		name: volume.Name,
+		url:  url,
 	}
 }
 
@@ -33,28 +33,41 @@ var modes = map[string]types.ReplicaMode{
 }
 
 func (c *controller) GetReplicaStates() ([]*types.ReplicaInfo, error) {
-	rs, err := c.client.ListReplicas()
-	if err != nil {
-		return nil, errors.Wrapf(err, "failed to get replicas from controller '%s'", c.name)
-	}
-	replicas := make([]*types.ReplicaInfo, len(rs))
-	for i, r := range rs {
-		replicas[i] = &types.ReplicaInfo{
-			InstanceInfo: types.InstanceInfo{
-				Address: r.Address,
-			},
-			Mode: modes[r.Mode],
+	replicas := []*types.ReplicaInfo{}
+	cancel := make(chan interface{})
+	defer close(cancel)
+	lineCh, errCh := util.CmdOutLines(exec.Command("longhorn", "--url", c.url, "ls"), cancel)
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for s := range lineCh {
+			if strings.HasPrefix(s, "ADDRESS") {
+				continue
+			}
+			fields := strings.Fields(s)
+			replicas = append(replicas, &types.ReplicaInfo{
+				InstanceInfo: types.InstanceInfo{
+					Address: fields[0],
+				},
+				Mode: modes[fields[1]],
+			})
 		}
+	}()
+	for err := range errCh {
+		return nil, err
 	}
+
+	wg.Wait()
 	return replicas, nil
 }
 
 func (c *controller) AddReplica(replica *types.ReplicaInfo) error {
-	err := sync.NewTask(c.url).AddReplica(replica.Address)
+	err := exec.Command("longhorn", "--url", c.url, "add", replica.Address).Run()
 	return errors.Wrapf(err, "failed to add replica address='%s' to controller '%s'", replica.Address, c.name)
 }
 
 func (c *controller) RemoveReplica(replica *types.ReplicaInfo) error {
-	_, err := c.client.DeleteReplica(replica.Address)
+	err := exec.Command("longhorn", "--url", c.url, "rm", replica.Address).Run()
 	return errors.Wrapf(err, "failed to rm replica address='%s' from controller '%s'", replica.Address, c.name)
 }
